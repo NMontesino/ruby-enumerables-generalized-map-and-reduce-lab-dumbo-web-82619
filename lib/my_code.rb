7808d0412de3f7c59d8b5953d13104144e849c9d
glorious-genre-8033 <namontesino@gmail.com>
2019-08-16 18:56:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,9 @@
 # Your Code Here
-array = [1,2,3]
 
 def map(array)
-  new = array
-  yield
-  return new
-end
-
-map(array) do |i|
-  i * -1
+  i = 0
+  while i < array.length do
+    yield
+  end
+  array
 end

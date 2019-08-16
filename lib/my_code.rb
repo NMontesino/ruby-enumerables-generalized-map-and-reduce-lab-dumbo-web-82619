c247f349675a34ec786c6b312d04c2ed28e284e2
glorious-genre-8033 <namontesino@gmail.com>
2019-08-16 22:01:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,10 +9,8 @@ def map(array)
 end
 
 def reduce(array, value = 0)
-  i = 0 
-  while i < array.length do
+  array.length.times do |i|
     value = yield(value, array[i])
-    i += 1
   end
   value
 end

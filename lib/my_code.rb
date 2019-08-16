713aa13afa2e625393243608e1808446a8de691f
glorious-genre-8033 <namontesino@gmail.com>
2019-08-16 19:29:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,19 +1,9 @@
 # Your Code Here
 
-# def map(array)
-#   i = 0 
-#   new_array = []
-#   while i < array.length do
-#     new_array.push(yield(array[i]))
-#     i += 1
-#   end
-#   new_array
-# end
-
 def map(array)
   new_array = []
-  array.length.times do |index|
-    new_array.push(yield(array[index]))
+  array.length.times do |i|
+    new_array.push(yield(array[i]))
   end
   new_array
 end
